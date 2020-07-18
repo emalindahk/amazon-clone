c1f368f2886c5422fddbfc2c95fe75382689b147
emalindahk <emalindahkimari@gmail.com>
2020-07-18 16:35:25 +0200

    feat(home.js home.css): created the Homepage Banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'; 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,15 +10,17 @@ function App() {
       <Router>
         <Switch>
           <Route path="/checkout">
+            <Header/>
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
+            <Header/>
             <h1>Login</h1>
           </Route>
           {/*Default route */}
           <Route path="/">
             <Header />
-            <h1>Home Page</h1>
+            <Home/>
           </Route>
         </Switch>
 
