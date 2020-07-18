ac6be1fbacc337b21edc014f5a816000b33d3ceb
emalindahk <emalindahkimari@gmail.com>
2020-07-18 22:18:06 +0200

    feat: added functionality to add  basket button

---
+++ src/reducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    basket: [],
+    user: null,
+}
+const reducer = (state, action) => {
+    
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            //Logic for adding stuff to basket
+            return {
+                 ...state,
+                 basket: [...state.basket, action.item]
+                };
+        case 'REMOVE_FROM_BASKET':
+            //Logic for removing item to basket
+            return { state };
+        default:
+            return state;
+    }
+}
+
+
+export default reducer;
