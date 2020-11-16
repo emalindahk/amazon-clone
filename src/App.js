84555a7e451a5034214ee6ee3fa089c275b87763
Emmah Kimari <emalindahkimari@gmail.com>
2020-11-16 05:04:17 +0300

    Add metadata

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { Helmet } from 'react-helmet';
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
@@ -35,6 +36,28 @@ function App() {
 
   return (
     <div className="App">
+      <Helmet>
+      <title>Amazon Clone</title>
+<meta name="description" content="Amazon Clone in ReactJs"/>
+
+{/* <!-- Google / Search Engine Tags --> */}
+<meta itemprop="name" content="Amazon Clone"/>
+<meta itemprop="description" content="Amazon Clone in ReactJs"/>
+<meta itemprop="image" content="https://live.staticflickr.com/65535/50607536646_c1e04e6f08_c.jpg"/>
+
+{/* <!-- Facebook Meta Tags --> */}
+<meta property="og:url" content="https://clone-a0ee9.firebaseapp.com"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="Amazon Clone"/>
+<meta property="og:description" content="Amazon Clone in ReactJs"/>
+<meta property="og:image" content="https://live.staticflickr.com/65535/50607536646_c1e04e6f08_c.jpg"/>
+
+{/* <!-- Twitter Meta Tags --> */}
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:title" content="Amazon Clone"/>
+<meta name="twitter:description" content="Amazon Clone in ReactJs"/>
+<meta name="twitter:image" content="https://live.staticflickr.com/65535/50607536646_c1e04e6f08_c.jpg"/> 
+      </Helmet>
       <Router>
         <Switch>
           <Route path="/checkout">
